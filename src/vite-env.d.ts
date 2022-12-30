2bcde74e21ababd9b3b5072bf0146d2db7f9d854
Taras Zherebetskiy <taras.zherebetskiy@echovox.com>
2022-12-30 20:36:25 +0200

    Added types & interfaces

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+type Colors = 'red' | 'green' | 'blue';
+type DateYMDString = `${YYYY}-${MM}-${DD}`;
+type ColorsClassName = `bg-${Colors}-300 text-${Colors}-900`;
+
+interface Item {
+  date: DateYMDString;
+  // ✓ TODO could we use stronger color type here?
+  color: Colors;
+}
+
+// ✓ TODO could we use inferred items type here?
+interface Range {
+  start: DateYMDString;
+  end: DateYMDString;
+  // ✓ TODO could we use stronger color type here?
+  color: Colors;
+}
